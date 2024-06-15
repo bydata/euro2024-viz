bf5e638b2878d8da2a82ca693784a81b634beaf8
bydata <ansgar.wolsing@gmail.com>
2024-06-15 16:23:09 +0200

    adapt the query so that only club infos will be considered even if multiple classes of results are returned (e.g. players)

--- R/retrieve_club_information_tm.R
+++ R/retrieve_club_information_tm.R
@@ -14,31 +14,38 @@ tm_build_query_url <- function(club_name) {
 }
 
 tm_query_club <- function(club_name, sleep = 1) {
+  club_name <- ifelse(club_name == "Brighton & Hove Albion", "Brighton", club_name)
   query_url <- tm_build_query_url(club_name)
   page <- read_html(query_url)
-  table_selector <- "div#yw0 > table"
-  raw_table <- html_node(page, table_selector) |> 
+  
+  # Extract all tables and find the result table for clubs
+  # (important in case the search query returns other result types, e.g. players)
+  table_selector <- "div table.items"
+  table_nodes <- html_nodes(page, table_selector)
+  node_texts <- map_chr(table_nodes, html_text)
+  which_table_node_id <- which(str_detect(node_texts, "ClubCountrySquad"))
+  
+  raw_table <- table_nodes[[which_table_node_id]] |>
     html_table()
   fmt_table <- raw_table[1, c(3,4, 7)]
   colnames(fmt_table) <- c("club_name", "club_league", "market_value")
-  
-  # In case the club name returned in the search results is different from the 
+
+  # In case the club name returned in the search results is different from the
   # one provided
   fmt_table$original_club_name <- club_name
-  
+
   # Get the country of the league
-  country <- html_nodes(page, "tbody td img")[2] |> 
+  country <- html_nodes(page, "tbody td img")[2] |>
     html_attr("title")
   fmt_table$league_country <- country
-  
+
   # Reorder columns
   fmt_table <- fmt_table[c(4, 1, 2, 5, 3)]
-  
+
   # Add some wait time after the query
   Sys.sleep(sleep)
-  
+
   fmt_table
-  
 }
 
 tm_query_club_possibly <- possibly(tm_query_club, otherwise = NULL)
@@ -47,13 +54,19 @@ clubs_info <- map(clubs$Club, tm_query_club_possibly)
 clubs_info <- set_names(clubs_info, clubs$Club)
 write_rds(clubs_info, here("data", "clubs-info.rds"))
 
+
+
+
 clubs_info |> 
   compact() |> 
   map(function(x) mutate(x, market_value = as.character(market_value))) |>
   bind_rows() |> 
-  filter(club_name != original_club_name) |> 
   View()
 
 tm_build_query_url("Újpest FC")
 tm_query_club("Újpest FC")
 tm_query_club("Borussia Dortmund")
+tm_query_club("AS Roma")
+
+
+
